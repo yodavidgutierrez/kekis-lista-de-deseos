61f38a08d83b2e8afefc5e110a85f0a17f9112d1
David Gutierrez <yodavidgutierrez@gmail.com>
2025-08-17 17:58:45 -0500

    second

---
+++ lista-deseos.py
@@ -0,0 +1,13 @@
+tareas = []
+
+def mostrarOpciones():
+    print("\n--- lista de deseos ---")
+    print("1. Añadir tarea")
+    print("2. Mostrar tareas")
+    print("3. Completar tarea")
+    print("4. Borrar tarea")
+    print("5. Salir")   
+
+while True:
+    mostrarOpciones()
+    break
